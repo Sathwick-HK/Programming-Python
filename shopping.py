18452a94958ff61ab5d0b4ae47ff113c7b2cd500
Sathwick-HK <58139626+Sathwick-HK@users.noreply.github.com>
2020-05-09 10:12:31 +0530

    Online Shopping
    Arun wants to buy a shirt. As he is very lazy, he decided to buy the
    shirt online. He chooses a shirt in Flipkart and is surprised to see the same shirt in Amazon and Snapdeal as well. So he decided to buy the shirt from the website which offers it at the least price. The price of the shirt, discount % and the shipping charges of all three websites have been given as input. Help him in calculating the price of the shirt in each website and decide which website has the lowest price.
    INPUT &amp; OUTPUT FORMAT:
    Input consist of 9 integers. First three input corresponds to Flipkart details such as the amount of the shirt, discount offered and shipping charges. Next three input corresponds to Snapdeal details such as the amount of shirt, discount offered and shipping charge. Last three input corresponds to amazon details such as the amount of shirt, discount offered and shipping charge.Write C++ program for showing the output amount and which brand he will prefer ?
    SAMPLE INPUT:
    
    1000
    
    50
    
    50
    
    900
    
    50
    
    70
    
    800
    
    10
    
    200
    
    SAMPLE OUTPUT:
    
    In Flipkart: Rs.550
    
    In Snapdeal: Rs.520
    
    In Amazon: Rs.920
    
    He will prefer Snapdeal

--- shopping.py
+++ shopping.py
@@ -1,21 +1,21 @@
-a = input()
-b = input()
-c = input()
-l = input()
-m = input()
-n = input()
-x = input()
-y = input()
-z = input()
-d1 = (a*b)/100
+a = int(input())
+b = int(input())
+c = int(input())
+l = int(input())
+m = int(input())
+n = int(input())
+x = int(input())
+y = int(input())
+z = int(input())
+d1 = int((a*b)/100)
 t1 = a-d1+c
-d2 = (l*m)/100
+d2 = int((l*m)/100)
 t2 = l-d2+n
-d3 = (x*y)/100
+d3 = int((x*y)/100)
 t3 = x-d3+z
-print("Flipkart Rs."+t1)
-print("Snapdeal Rs."+t2)
-print("Amazon Rs."+t3)
+print("Flipkart Rs."+str(t1))
+print("Snapdeal Rs."+str(t2))
+print("Amazon Rs."+str(t3))
 if(t1<=t2 & t1<=t3):
     print("He will prefer Flipkart")
 elif(t2<=t3 & t2<=t1):
